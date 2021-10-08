50d2dd7ffb9887a6811a9e0eebd9fdcebac1736e
BiancaPalileo <biclpalileo@gmail.com>
2021-10-08 12:20:31 +0100

    Lab 2 Finished

--- GE1 Examples 2021/Assets/AITank.cs
+++ GE1 Examples 2021/Assets/AITank.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class AITank : MonoBehaviour {
 
     public float radius = 10;
-    public int numWaypoints = 5;
+    public int numWaypoints = 6;
     public int current = 0;
     List<Vector3> waypoints = new List<Vector3>();
     public float speed = 10;
-    public Transform player;    
+    public Transform player;
+    public float rotSpeed = 180f;
 
     public void OnDrawGizmos()
     {
@@ -22,6 +23,19 @@ public class AITank : MonoBehaviour {
             // You can draw gizmos using
             // Gizmos.color = Color.green;
             // Gizmos.DrawWireSphere(pos, 1);
+            float theta = (2.0f * Mathf.PI) / (float) numWaypoints;
+
+            for(int i = 0; i < numWaypoints; i++) {
+
+                float angle = theta * i;
+                float x = Mathf.Sin(angle) * radius;
+                float z = Mathf.Cos(angle) * radius;
+
+                Vector3 pos = new Vector3(x, 0.5f, z);
+
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireSphere(pos, 1);
+            }
         }
     }
 
@@ -30,6 +44,20 @@ public class AITank : MonoBehaviour {
         // Task 2
         // Put code here to calculate the waypoints in a loop and 
         // Add them to the waypoints List
+        float theta = (2.0f * Mathf.PI) / (float) numWaypoints;
+
+        for(int i = 0; i < numWaypoints; i++) {
+
+            float angle = theta * i;
+            float x = Mathf.Sin(angle) * radius;
+            float z = Mathf.Cos(angle) * radius;
+
+            Vector3 pos = new Vector3(x, 0.5f, z);
+
+            waypoints.Add(pos);
+
+            // Debug.Log(waypoints[i]);
+        }
     }
 
     // Update is called once per frame
@@ -37,13 +65,48 @@ public class AITank : MonoBehaviour {
         // Task 3
         // Put code here to move the tank towards the next waypoint
         // When the tank reaches a waypoint you should advance to the next one
+        Quaternion rotTarget = Quaternion.LookRotation(waypoints[current] - transform.position);
 
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotTarget, rotSpeed * Time.deltaTime);
+
+        if(transform.rotation == rotTarget) {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if(Vector3.Distance(waypoints[current], transform.position) < 1f) {
+            current++;
+        }
+
+        if(current == waypoints.Count) {
+            current = 0;
+        }
 
         // Task 4
-        // Put code here to check if the player is in front of or behine the tank
+        // Put code here to check if the player is in front of or behind the tank
+        Vector3 distance = transform.position - player.position;
+        float dot = Vector3.Dot(transform.forward, distance);
+        
+        if(dot > 0) {
+            GameManager.Log("The player is in front");
+        }
+        else if(dot < 0) {
+            GameManager.Log("The player is behind!");
+        }
+
         // Task 5
         // Put code here to calculate if the player is inside the field of view and in range
         // You can print stuff to the screen using:
-        GameManager.Log("Hello from th AI tank");
+        // GameManager.Log("Hello from the AI tank");
+        float angle = Mathf.Acos(dot / distance) * Mathf.RadtoDeg;
+        float distanceFloat = Vector3.Distance(transform.position, player.position);
+
+        if(angle < 45 && distanceFloat < 10f){
+            GameManager.Log("Enemy spotted!");
+        }
+        else {
+            GameManager.Log("The enemy is hidden!");
+        }
+        
     }
 }
+
